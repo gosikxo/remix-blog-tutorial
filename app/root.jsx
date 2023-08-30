c8443965f4519dbd092a6a2e4771515504849fe7
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-08-30 15:10:49 +0200

    Init project, add basic structure and styles

---
+++ app/root.jsx
@@ -0,0 +1,54 @@
+import { Outlet, LiveReload, Link, Links } from "@remix-run/react"
+import globalStylesUrl from "~/styles/global.css"
+
+export const links = () => [{ rel: "stylesheet", href: globalStylesUrl }]
+
+// export const meta: MetaFunction = () => ({
+//   description: "A cool blog built with Remix",
+//   keywords: "remix, react, javascript",
+// })
+
+export default function App() {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  )
+}
+
+function Document({ children, title }) {
+  return (
+    <html lang="en">
+      <head>
+        {/* <Meta /> */}
+        <Links />
+        <title>{title ? title : "Remix Blog"}</title>
+      </head>
+      <body>
+        {children}
+        {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
+      </body>
+    </html>
+  )
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          Remix
+        </Link>
+        <ul className="nav">
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="container">{children}</div>
+    </>
+  )
+}
