f1f7b1d5f4c1e38c295692d927a0a6809b1788a4
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-08-30 17:30:29 +0200

    Add PostItems component

--- app/routes/posts.jsx
+++ app/routes/posts.jsx
@@ -2,10 +2,9 @@ import { Outlet } from "@remix-run/react"
 
 function Posts() {
   return (
-    <div>
-      <h1>This is the posts route</h1>
+    <>
       <Outlet />
-    </div>
+    </>
   )
 }
 
