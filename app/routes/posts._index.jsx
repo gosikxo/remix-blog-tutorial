77ecddb199132c2cd9250832c05ae8af0ab0c2d7
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-09-01 12:51:00 +0200

    Add new post component

--- app/routes/posts._index.jsx
+++ app/routes/posts._index.jsx
@@ -1,8 +1,37 @@
+import { Link, useLoaderData } from "@remix-run/react"
+
+export const loader = () => {
+  const data = {
+    posts: [
+      { id: 1, title: "Post 1", body: "This is post 1" },
+      { id: 2, title: "Post 2", body: "This is post 2" },
+      { id: 3, title: "Post 3", body: "This is post 3" },
+    ],
+  }
+  return data
+}
+
 function PostItems() {
+  const { posts } = useLoaderData()
+
   return (
-    <div>
-      <h1>Posts</h1>
-    </div>
+    <>
+      <div className="page-header">
+        <h1>Posts</h1>
+        <Link to="new" className="btn">
+          New Post
+        </Link>
+      </div>
+      <ul className="posts-list">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={post.id}>
+              <h3>{post.title}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
