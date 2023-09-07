21f53563d864a453a186c56e910cd58c974925e9
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-09-07 15:38:18 +0200

    Add login form

--- app/root.jsx
+++ app/root.jsx
@@ -47,6 +47,9 @@ function Layout({ children }) {
           <li>
             <Link to="/posts">Posts</Link>
           </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
         </ul>
       </nav>
 
---
+++ app/routes/login.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+
+function Login() {
+  return (
+    <div className="auth-container">
+      <div className="page-header">
+        <h1>Login</h1>
+      </div>
+
+      <div className="page-content">
+        <form method="POST">
+          <fieldset>
+            <legend>Login or Register</legend>
+            <label>
+              <input type="radio" name="loginType" value="login" />
+              Login
+            </label>
+            <label>
+              <input type="radio" name="loginType" value="register" />
+              Register
+            </label>
+          </fieldset>
+          <div className="form-control">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" />
+            <div className="error"></div>
+          </div>
+          <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+            <div className="error"></div>
+          </div>
+          <button className="btn btn-block" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
