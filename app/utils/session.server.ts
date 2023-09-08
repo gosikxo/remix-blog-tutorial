e303bcfe09b2aa4708c84d788ca55424cc3564ca
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-09-08 14:42:55 +0200

    Get user

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -51,3 +51,24 @@ export async function createUserSession(userId: string, redirectTo: string) {
     },
   })
 }
+//Get user session
+export function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get("Cookie"))
+}
+//Get logged in user
+export async function getUser(request: Request) {
+  const session = await getUserSession(request)
+  const userId = session.get("userId")
+  if (!userId || typeof userId !== "string") return null
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        id: userId,
+      },
+    })
+    return user
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
